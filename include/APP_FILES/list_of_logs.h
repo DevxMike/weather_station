2fd8ea6f55bf45c6562dfe700adfa0836a084a3c
mike <michalbaz@poczta.onet.pl>
2023-04-16 21:58:19 +0200

    added implementation of list of temperature logs

--- include/APP_FILES/list_of_logs.h
+++ include/APP_FILES/list_of_logs.h
@@ -1,4 +1,36 @@
-#infdef list_of_logs_h
+#ifndef list_of_logs_h
 #define list_of_logs_h
+
+#include "APP_FILES/list_of_logs.h"
+#include "APP_FILES/main.h"
+
+#define MAX_LINES 10
+
+typedef struct temperature_log{
+  int16_t temperature;
+  int16_t time_ago;
+  struct temperature_log* next;
+};
+
+class list_of_logs{
+private:
+  temperature_log* head;
+  const uint8_t max_count;
+  uint8_t count;
+
+public:
+  list_of_logs():
+    max_count{ MAX_LINES }, head{ nullptr }, count{ 0 }{}
+  ~list_of_logs(){
+    clear_list();
+  }
+
+  void append(int16_t temperature, uint8_t interval);
+
+  void clear_list();
+
+  temperature_log* get_elements();
+};
+
 
 #endif
---
+++ src/APP_FILES/list_of_logs.cpp
@@ -0,0 +1,68 @@
+#include "APP_FILES/list_of_logs.h"
+
+static const int16_t intervals[3]{
+    10, 30, 60
+};
+
+void list_of_logs::append(int16_t temperature, uint8_t interval){
+    if(head == nullptr){
+      temperature_log* tmp = new temperature_log;
+      tmp->next = nullptr;
+      tmp->temperature = temperature;
+      tmp->time_ago = 0;
+      head = tmp;
+
+      ++count;
+    }
+    else if(count < max_count){
+      temperature_log* tmp = new temperature_log;
+      tmp->next = nullptr;
+
+      tmp->temperature = temperature;
+      tmp->time_ago = 0;
+
+      temperature_log* ptr = head;
+
+      while(ptr->next != nullptr){
+        ptr->time_ago -= intervals[interval];
+        ptr = ptr->next;
+      }
+
+      ptr->next = tmp;
+      ++count;
+    }
+    else{
+      temperature_log* tmp = new temperature_log;
+      tmp->next = nullptr;
+
+      tmp->temperature = temperature;
+      tmp->time_ago = 0;
+
+      temperature_log* ptr = head->next;
+      delete head;
+      head = ptr;
+
+      while(ptr->next != nullptr){
+        ptr->time_ago -= intervals[interval];
+        ptr = ptr->next;
+      }
+
+      ptr->next = tmp;
+    }
+  }
+
+  void list_of_logs::clear_list(){
+    count = 0;
+
+    temperature_log* tmp = head;
+
+    while(tmp != nullptr){
+      tmp = tmp->next;
+      delete head;
+      head = tmp;
+    }
+  }
+
+  temperature_log* list_of_logs::get_elements(){
+    return head;
+  }
