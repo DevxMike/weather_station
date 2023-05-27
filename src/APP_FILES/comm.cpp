cda6352cd4e6fbe99a62081c391756af596d0deb
DevxMike <michalbaz@poczta.onet.pl>
2023-05-27 19:56:46 +0200

    .

---
+++ src/APP_FILES/comm.cpp
@@ -0,0 +1,141 @@
+#include "APP_FILES/comm.h"
+
+static const char *root_ca PROGMEM = R"EOF(
+-----BEGIN CERTIFICATE-----
+MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw
+TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh
+cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMTUwNjA0MTEwNDM4
+WhcNMzUwNjA0MTEwNDM4WjBPMQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJu
+ZXQgU2VjdXJpdHkgUmVzZWFyY2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBY
+MTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAK3oJHP0FDfzm54rVygc
+h77ct984kIxuPOZXoHj3dcKi/vVqbvYATyjb3miGbESTtrFj/RQSa78f0uoxmyF+
+0TM8ukj13Xnfs7j/EvEhmkvBioZxaUpmZmyPfjxwv60pIgbz5MDmgK7iS4+3mX6U
+A5/TR5d8mUgjU+g4rk8Kb4Mu0UlXjIB0ttov0DiNewNwIRt18jA8+o+u3dpjq+sW
+T8KOEUt+zwvo/7V3LvSye0rgTBIlDHCNAymg4VMk7BPZ7hm/ELNKjD+Jo2FR3qyH
+B5T0Y3HsLuJvW5iB4YlcNHlsdu87kGJ55tukmi8mxdAQ4Q7e2RCOFvu396j3x+UC
+B5iPNgiV5+I3lg02dZ77DnKxHZu8A/lJBdiB3QW0KtZB6awBdpUKD9jf1b0SHzUv
+KBds0pjBqAlkd25HN7rOrFleaJ1/ctaJxQZBKT5ZPt0m9STJEadao0xAH0ahmbWn
+OlFuhjuefXKnEgV4We0+UXgVCwOPjdAvBbI+e0ocS3MFEvzG6uBQE3xDk3SzynTn
+jh8BCNAw1FtxNrQHusEwMFxIt4I7mKZ9YIqioymCzLq9gwQbooMDQaHWBfEbwrbw
+qHyGO0aoSCqI3Haadr8faqU9GY/rOPNk3sgrDQoo//fb4hVC1CLQJ13hef4Y53CI
+rU7m2Ys6xt0nUW7/vGT1M0NPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNV
+HRMBAf8EBTADAQH/MB0GA1UdDgQWBBR5tFnme7bl5AFzgAiIyBpY9umbbjANBgkq
+hkiG9w0BAQsFAAOCAgEAVR9YqbyyqFDQDLHYGmkgJykIrGF1XIpu+ILlaS/V9lZL
+ubhzEFnTIZd+50xx+7LSYK05qAvqFyFWhfFQDlnrzuBZ6brJFe+GnY+EgPbk6ZGQ
+3BebYhtF8GaV0nxvwuo77x/Py9auJ/GpsMiu/X1+mvoiBOv/2X/qkSsisRcOj/KK
+NFtY2PwByVS5uCbMiogziUwthDyC3+6WVwW6LLv3xLfHTjuCvjHIInNzktHCgKQ5
+ORAzI4JMPJ+GslWYHb4phowim57iaztXOoJwTdwJx4nLCgdNbOhdjsnvzqvHu7Ur
+TkXWStAmzOVyyghqpZXjFaH3pO3JLF+l+/+sKAIuvtd7u+Nxe5AW0wdeRlN8NwdC
+jNPElpzVmbUq4JUagEiuTDkHzsxHpFKVK7q4+63SM1N95R1NbdWhscdCb+ZAJzVc
+oyi3B43njTOQ5yOf+1CceWxG1bQVs5ZufpsMljq4Ui0/1lvh+wjChP4kqKOJ2qxq
+4RgqsahDYVvTH9w7jXbyLeiNdd8XM2w9U/t7y0Ff/9yi0GE44Za4rF2LN9d11TPA
+mRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57d
+emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=
+-----END CERTIFICATE-----
+)EOF";
+
+WiFiClientSecure espClient;
+PubSubClient mqttClient(espClient);
+
+const char* mqtt_broker = "937f7c4f4ebe47ffaa73e3f12dc08165.s2.eu.hivemq.cloud";
+const char* mqtt_user_name = "WeatherStation";
+const char* mqtt_password = "ws17bezstA";
+
+void comm::callback(char* topic, byte* payload, unsigned int length) {
+  // MQTT message callback function
+}
+
+bool comm::connect(){
+    espClient.setCACert(root_ca);
+    mqttClient.setServer(mqtt_broker, 8883);
+
+    auto tmp = mqttClient.connect("Weather", mqtt_user_name, mqtt_password);
+
+    if(mqttClient.connected()){
+        mqttClient.setCallback(callback);
+
+        mqttClient.subscribe("ws_init");
+        mqttClient.subscribe("ws_settings");
+    }
+    else{
+
+    }
+
+    return tmp;
+}
+
+void comm::send_message(const char* topic, const char* mesg){
+    // mqttClient.publish(topic, mesg);
+    messages.push(topic, mesg);
+}
+
+void comm::main(){
+    static uint8_t comm_state = 0;
+    static unsigned long timer = 0;
+    msg_t message;
+
+    mqttClient.loop();
+
+    switch(comm_state){
+        case 0:
+            if(mqttClient.connected()){
+                comm_state = 2;
+            }
+            else{
+                mqttClient.connect("Weather", mqtt_user_name, mqtt_password);
+                timer = millis();
+                comm_state = 1;
+            }
+        break;
+
+        case 1:
+            if(millis() - timer >= 500){
+                if(mqttClient.connected()){
+                    comm_state = 2;
+                }
+                else{
+                    timer = millis();
+                    comm_state = 0;
+                }
+            }
+        break;
+
+        case 2:
+            if(mqttClient.connected()){
+                    while(!messages.is_empty()){
+                        messages.pop(message);
+                        mqttClient.publish(
+                            message.topic.c_str(), 
+                            message.payload.c_str()
+                        );
+
+                        Serial.println(message.topic.c_str());
+                        Serial.println(message.payload.c_str());
+
+                    }
+
+                }
+                else{
+                    comm_state = 0;
+                }
+        break;
+    }
+}
+
+void message_stack::push(String topic, String payload){
+    if(!is_full()){
+        messages[count].payload = payload;
+        messages[count].topic = topic;
+        ++count;
+    }
+    else{
+
+    }
+}
+
+void message_stack::pop(msg_t& msg){
+    if(!is_empty()){
+        msg = messages[count - 1];
+        --count;
+    }
+}
